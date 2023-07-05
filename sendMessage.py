3d85e26df05e1687845802e2fd3e492acc9654a6
Gante1a <ilya-goncharov2003@mail.ru>
2023-07-05 14:18:40 +0300

    реализация отправки сообщений  без сторонней библиотеки

--- sendMessage.py
+++ sendMessage.py
@@ -1,26 +1,22 @@
 import json
-from telegram import Bot
+import requests
 from jinja2 import Template
 from database import DatabaseConnector
-import asyncio
 
 class MessageSender:
     def __init__(self, config_file='config.json'):
-        # Открытие JSON файла
         with open(config_file, 'r', encoding='utf-8') as file:
             config_data = json.load(file)
         
         self.bot_token = config_data['bot_token']
         self.template = Template(config_data['template_message'])
-        self.telegramBot = Bot(token=self.bot_token)
         self.db_connector = DatabaseConnector()
+        self.send_message()
 
-    async def send_message(self):
-        # Запрос ввода данных из консоли
-        text_id = input('Введите text_id пользователя: ')
-        message = input('Введите сообщение для отправки: ')
+    def send_message(self):
+        text_id = input('Введите text_id пользователя: \n')
+        message = input('Введите сообщение для отправки: \n')
 
-        # Выбор chat_id по chat_id
         sql = f"SELECT chat_id FROM users WHERE chat_id = '{text_id}'"
 
         connection = self.db_connector.connect()
@@ -29,26 +25,23 @@ class MessageSender:
         rows = cursor.fetchall()
         message_text = self.template.render(message=message)
 
-        # Если введённые данные ошибочны, сообщение не отправится
         if len(rows) == 0:
             print('Введённые данные некорректные, повторите ввод')
-            await self.send_message()
+            self.send_message()
             return
 
-        # Перебор результатов запроса для отправки сообщения
         for row in rows:
             user_chat_id = row[0]
-            self.telegramBot.send_message(chat_id=user_chat_id, text=message_text)
+            send_message_url = f"https://api.telegram.org/bot{self.bot_token}/sendMessage"
+            payload = {
+                'chat_id': user_chat_id,
+                'text': message_text
+            }
+            response = requests.post(send_message_url, json=payload)
+            if response.status_code == 200:
+                print('Сообщение было успешно отправлено пользователю')
 
         cursor.close()
         connection.close()
-        print('Сообщение было успешно отправлено пользователю')
 
-    # Запуск асинхронной функции
-    def run(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.send_message())
-
-# Использование класса
 sender = MessageSender()
-sender.run()
