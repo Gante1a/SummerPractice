2d08b318ff82a050d9864be834f841aff7f2d179
Gante1a <ilya-goncharov2003@mail.ru>
2023-07-19 15:53:12 +0300

    Добавлена обработка exception-ов

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sendMessage import MessageSender
-from databaseOperations import getInfo, insert_key, DatabaseError
+from databaseOperations import getInfo, insert_key
 
 app = FastAPI()
 
@@ -14,9 +14,9 @@ async def show_form_and_data(request: Request):
         # Обработка вывода данных о пользователях
         users_data = getInfo()
         return templates.TemplateResponse("form.html", {"request": request, "combined_user_data": users_data})
-    except DatabaseError as e:
-        return templates.TemplateResponse("error.html", {"request": request, "error_message": str(e)})
-
+    except Exception as e:
+        return templates.TemplateResponse("error.html", {"request": request, "error_message": "Произошла ошибка: {str(e)}"})
+    
 @app.post("/", response_class=HTMLResponse)
 async def process_form(request: Request, chat_id: str = Form(None), message: str = Form(None), key: str = Form(None), official_name: str = Form(None)):
     try:
@@ -27,7 +27,5 @@ async def process_form(request: Request, chat_id: str = Form(None), message: str
             # Обработка добавления ключа в базу данных
             insert_key(key, official_name)
         return RedirectResponse(url="/", status_code=303)
-    except DatabaseError as e:
-        return templates.TemplateResponse("error.html", {"request": request, "error_message": str(e)})
     except Exception as e:
-        return templates.TemplateResponse("error.html", {"request": request, "error_message": "Произошла ошибка"})
+        return templates.TemplateResponse("error.html", {"request": request, "error_message": "Произошла ошибка: {str(e)}"})
