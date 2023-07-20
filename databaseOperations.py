6d6ba877e5ad69cc21191cb2d5f7437d4f6d9c0f
Gante1a <ilya-goncharov2003@mail.ru>
2023-07-20 15:04:44 +0300

    добавлено удаление из базы

--- databaseOperations.py
+++ databaseOperations.py
@@ -1,7 +1,7 @@
 from database import DatabaseManager, UserMain, UserOptional, UserKeys
 from sqlalchemy.exc import SQLAlchemyError
 
-def getInfo():
+def getUsersInfo():
     '''Вывод инфориации о пользователях'''
     try:
         db_manager = DatabaseManager()
@@ -48,3 +48,65 @@ def insert_key(key: str, official_name: str):
         raise Exception(f"Ошибка базы данных при добавлении ключа: {str(e)}")
     finally:
         db_manager.close()
+
+from sqlalchemy.orm.exc import NoResultFound
+
+def delete_user(chat_id):
+    '''Удаление пользователя из таблиц users_main и users_optional по chat_id'''
+    try:
+        db_manager = DatabaseManager()
+        session = db_manager.session
+        try:
+            user_main_data = session.query(UserMain).filter(UserMain.chat_id == chat_id).one()
+            user_optional_data = session.query(UserOptional).filter(UserOptional.chat_id == chat_id).one()
+
+            session.delete(user_main_data)
+            session.delete(user_optional_data)
+            session.commit()
+        except NoResultFound:
+            pass
+        except Exception as e:
+            session.rollback()
+            raise Exception(f"Ошибка базы данных при удалении пользователя: {str(e)}")
+    finally:
+        db_manager.close()
+
+def getKeysInfo():
+    '''Get information from the keys table'''
+    try:
+        db_manager = DatabaseManager()
+        session = db_manager.session
+        result = []
+
+        try:
+            keys_data = session.query(UserKeys.key, UserKeys.official_name).all()
+
+            for key_row in keys_data:
+                key = key_row.key
+                official_name = key_row.official_name or ""
+                result.append((key, official_name))
+
+            return result
+
+        finally:
+            db_manager.close()
+    except SQLAlchemyError as e:
+        raise Exception(f"Database error: {str(e)}")  
+
+def delete_key(key):
+    '''Delete a key from the keys table'''
+    try:
+        db_manager = DatabaseManager()
+        session = db_manager.session
+        try:
+            key_data = session.query(UserKeys).filter(UserKeys.key == key).one()
+            session.delete(key_data)
+            session.commit()
+        except NoResultFound:
+            pass
+        except Exception as e:
+            session.rollback()
+            raise Exception(f"Database error while deleting key: {str(e)}")
+    finally:
+        db_manager.close()          
+
--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from sendMessage import MessageSender
-from databaseOperations import getInfo, insert_key
+from databaseOperations import getUsersInfo, insert_key, delete_user, getKeysInfo, delete_key
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="")
-
-@app.get("/", response_class=HTMLResponse)
-async def show_form_and_data(request: Request):
-    try:
-        # Обработка вывода данных о пользователях
-        users_data = getInfo()
-        return templates.TemplateResponse("form.html", {"request": request, "combined_user_data": users_data})
-    except Exception as e:
-        return templates.TemplateResponse("error.html", {"request": request, "error_message": "Произошла ошибка: {str(e)}"})
     
 @app.post("/", response_class=HTMLResponse)
-async def process_form(request: Request, chat_id: str = Form(None), message: str = Form(None), key: str = Form(None), official_name: str = Form(None)):
+async def process_form(chat_id: str = Form(None), message: str = Form(None), key: str = Form(None), official_name: str = Form(None)):
     try:
         if chat_id and message:
-            '''Обработка отправки сообщения с помощью sendMessage.py'''
             await MessageSender(chat_id, message).send_message()
         elif key and official_name:
-            # Обработка добавления ключа в базу данных
             insert_key(key, official_name)
         return RedirectResponse(url="/", status_code=303)
     except Exception as e:
-        return templates.TemplateResponse("error.html", {"request": request, "error_message": "Произошла ошибка: {str(e)}"})
+        return JSONResponse(content={"error_message": f"Error: {str(e)}"}, status_code=500)
+    
+@app.post("/delete", response_class=HTMLResponse)
+async def delete_user_by_chat_id(chat_id: str = Form(...)):
+    try:
+        delete_user(chat_id)
+        return RedirectResponse(url="/", status_code=303)
+    except Exception as e:
+        return JSONResponse(content={"error_message": f"Error: {str(e)}"}, status_code=500)
+ 
+@app.get("/", response_class=HTMLResponse)
+async def show_form_and_data(request: Request):
+    try:
+        users_data = getUsersInfo()
+        keys_data = getKeysInfo()
+        return templates.TemplateResponse("form.html", {"request": request, "combined_user_data": users_data, "keys_data": keys_data})
+    except Exception as e:
+        return JSONResponse(content={"error_message": f"Error: {str(e)}"}, status_code=500)
+    
+@app.post("/delete-key", response_class=HTMLResponse)
+async def delete_key(key: str = Form(...)):
+    try:
+        delete_key(key)
+        return RedirectResponse(url="/", status_code=303)
+    except Exception as e:
+        return JSONResponse(content={"error_message": f"Error: {str(e)}"}, status_code=500)   
